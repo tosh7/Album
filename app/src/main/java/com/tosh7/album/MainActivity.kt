f411f6db158e4bbac2ecd53a6e795508fc1c093e
tosh7 <zlia.6.lj.425@gmail.com>
2019-10-29 18:37:13 +0900

    done

--- app/src/main/java/com/tosh7/album/MainActivity.kt
+++ app/src/main/java/com/tosh7/album/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.tosh7.album
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, PreviewActivity::class.java)
+
+        imageView.setOnClickListener {
+            intent.putExtra("image", R.drawable.ocean)
+            startActivity(intent)
+        }
+
+        imageView2.setOnClickListener {
+            intent.putExtra("image", R.drawable.portugal)
+            startActivity(intent)
+        }
+
+        imageView3.setOnClickListener {
+            intent.putExtra("image", R.drawable.sakura)
+            startActivity(intent)
+        }
+
+        imageView4.setOnClickListener {
+            intent.putExtra("image", R.drawable.ship)
+            startActivity(intent)
+        }
+
+        imageView5.setOnClickListener {
+            intent.putExtra("image", R.drawable.waterfall)
+            startActivity(intent)
+        }
+
+        imageView6.setOnClickListener {
+            intent.putExtra("image", R.drawable.yukata)
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/java/com/tosh7/album/PreviewActivity.kt
+++ app/src/main/java/com/tosh7/album/PreviewActivity.kt
@@ -2,11 +2,19 @@ package com.tosh7.album
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_preview.*
 
 class PreviewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
+
+        val imageId = intent.getIntExtra("image", 0)
+        preview_image.setImageResource(imageId)
+
+        back_button.setOnClickListener {
+            finish()
+        }
     }
 }
